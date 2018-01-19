919141ccec240bd21e4d5b6be3dbaef1a9773d55
Niharika04 <niharika0217@gmail.com>
2018-01-19 14:39:06 -0800

    add files

---
+++ assets/javascript/game.js
@@ -0,0 +1,19 @@
+// var wordBank = ["red", "blue", "yellow"];
+// var wins = 0;
+// var loss = 0;
+// var wrongLetter = [];
+// var guessesLeft = 15;
+// var underScores = [];
+// var correctGuess =[];
+// var randWord
+
+// function startGame() {
+//  rendWord = wordBank[Math.floor(Math.random() * wordbank.length)];
+
+//  for (var i = 0; i < randWord.length; i++ ) {
+//  	underScores.push('_')
+//  }
+// }
+// start hangman game! 
+
+
